eb00c86e2898a65219b41354315ca48b54b4640a
Xue <1099232082@qq.com>
2019-06-10 15:46:10 +0800

    first commit

---
+++ shop-client/src/router/index.js
@@ -0,0 +1,57 @@
+/**
+ * 路由器对象模块
+ */
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from '../pages/Home/Home.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Person from '../pages/Person/Person.vue'
+import Login from '../pages/Login/Login.vue'
+
+// 声明使用插件
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  // 配置所有路由
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      component: Home,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/person',
+      component: Person,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
+})
